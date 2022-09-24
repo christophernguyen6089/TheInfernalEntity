d987c84dc4bf091723f782568a4d7e3175ca690d
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-09-24 11:52:23 -0500

    Character Health and death

---
+++ NegativeSpaceProject/Assets/charUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class charUnit : MonoBehaviour
+{
+
+    public float playerMaxHealth = 10f;
+    public float currentPlayerHealth = 10f;
+    //based on napkin math, 1f is roughly equal to 1 second, so this will give about 10 seconds of leeway... i think
+
+    public Slider healthSlider;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider.maxValue = 10f;
+        healthSlider.minValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPlayerHealth-=Time.deltaTime;
+        healthSlider.value = currentPlayerHealth;
+
+        if(currentPlayerHealth<=0f){
+           characterMove.DebugLogPrint("Goodbye, world.");
+            gameObject.SetActive(false);
+        }
+    }
+}
--- NegativeSpaceProject/Assets/characterMove.cs
+++ NegativeSpaceProject/Assets/characterMove.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class characterMove : MonoBehaviour
 {
+    
+
     public Rigidbody2D playerBody;
     private Vector2 moveDirection = Vector2.zero;
 
+    
+
     private Camera ourCamera;
-    //public Collider2D theirCollider;
     private float xMovement, yMovement;
     
     // Start is called before the first frame update
