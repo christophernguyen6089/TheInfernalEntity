00508c564045ffd29c01806835b72a1c13faa10d
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-09-24 00:18:41 -0500

    I did it
    I made movement

---
+++ NegativeSpaceProject/Assets/characterMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMove : MonoBehaviour
+{
+    public Rigidbody2D playerBody;
+    private Vector2 moveDirection = Vector2.zero;
+
+    private Camera ourCamera;
+    private float xMovement, yMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourCamera = Camera.main;
+    }
+
+    void Update(){
+        xMovement=Input.GetAxis("Horizontal");
+        yMovement=Input.GetAxis("Vertical");
+        playerBody.velocity = new Vector2(xMovement*25f, yMovement*25f);
+    }
+}
