734f1c53a80c73c4635f0beb6f61ae540d7f5927
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-09-24 12:58:42 -0500

    added sprinting function

--- NegativeSpaceProject/Assets/charUnit.cs
+++ NegativeSpaceProject/Assets/charUnit.cs
@@ -9,9 +9,12 @@ public class charUnit : MonoBehaviour
 
     public float playerMaxHealth = 10f;
     public float currentPlayerHealth = 10f;
+    public float decreaseParameter=1;
     //based on napkin math, 1f is roughly equal to 1 second, so this will give about 10 seconds of leeway... i think
 
     public Slider healthSlider;
+
+    
     
 
     // Start is called before the first frame update
@@ -28,7 +31,7 @@ public class charUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentPlayerHealth-=Time.deltaTime;
+        currentPlayerHealth-=Time.deltaTime*decreaseParameter;
         healthSlider.value = currentPlayerHealth;
 
         if(currentPlayerHealth<=0f){
--- NegativeSpaceProject/Assets/characterMove.cs
+++ NegativeSpaceProject/Assets/characterMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class characterMove : MonoBehaviour
 {
-    
+    public charUnit unit;
 
     public Rigidbody2D playerBody;
     private Vector2 moveDirection = Vector2.zero;
@@ -23,7 +23,18 @@ public class characterMove : MonoBehaviour
     private void Update(){
         xMovement=Input.GetAxis("Horizontal");
         yMovement=Input.GetAxis("Vertical");
-        playerBody.velocity = new Vector2(xMovement*10f, yMovement*10f);
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            playerBody.velocity = new Vector2(xMovement*15f, yMovement*15f);
+            unit.decreaseParameter=1.5f;
+        }
+        else{
+            playerBody.velocity = new Vector2(xMovement*10f, yMovement*10f);
+            unit.decreaseParameter=1f;
+        }
+        
+
+        
     }
 
     public static void DebugLogPrint(string message){
