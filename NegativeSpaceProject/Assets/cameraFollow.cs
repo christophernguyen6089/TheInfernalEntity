5fb3118cdf84e8da642717196f65330469fea386
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-09-24 16:25:31 -0500

    made a basic spawner, TODO: Movements of items

--- NegativeSpaceProject/Assets/cameraFollow.cs
+++ NegativeSpaceProject/Assets/cameraFollow.cs
@@ -6,13 +6,13 @@ using TMPro;
 
 public class cameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
+    private float FollowSpeed = 5f;
     public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -100f);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, newPos, FollowSpeed*Time.deltaTime);
         transform.position = smoothedPosition;
     }
---
+++ NegativeSpaceProject/Assets/fountainSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fountainSpawner : MonoBehaviour
+{
+    private float radius;
+    private float spawnInterval;
+    private float spawnCountdown;
+    private float decreaseParameter;
+    private Vector3 area;
+
+
+    
+    //hard coded cause lol
+
+    
+    public GameObject healthPickup;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius=3f;
+        spawnCountdown=2f;
+        decreaseParameter=1f;
+
+        Instantiate(healthPickup, new Vector3(0,0,0), transform.rotation);
+        //BE AWARE OF HARDCODED COORDINATES
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnCountdown-=Time.deltaTime*decreaseParameter;
+        if(spawnCountdown<=0){
+            area = new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius), 0);
+            Instantiate(healthPickup, area, transform.rotation);
+            //BE AWARE OF HARDCODED COORDINATES
+            spawnCountdown=1f;
+        }
+    }
+}
